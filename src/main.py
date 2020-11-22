fc2a37163461d57df560c1d90e5e170170254d16
Hi-im-Yan <yanajiki2010@hotmail.com>
2020-11-22 18:30:15 -0400

    Criando tabela candidato

--- src/main.py
+++ src/main.py
@@ -4,6 +4,39 @@ import json
 with open('credentials.json', 'r') as c:
     credentials = json.load(c)
 
+with open('../candidatos/120000633197.json', 'r') as candidate:
+    c_infos = json.load(candidate)
+
+
+attr = ''
+attr_type = ''
+to_query = ''
+for info in c_infos:
+    attr = info
+    if type(c_infos[info]) == bool:
+        attr_type = 'BOOLEAN NOT NULL'
+        to_query += attr + " " + attr_type + ", "
+    elif type(c_infos[info]) == int:
+        attr_type = 'INTEGER NOT NULL'
+        to_query += attr + " " + attr_type + ", "
+    elif type(c_infos[info]) == str:
+        attr_type = 'VARCHAR NOT NULL'
+        to_query += attr + " " + attr_type + ", "
+    elif type(c_infos[info]) == float:
+        attr_type = 'FLOAT NOT NULL'
+        to_query += attr + " " + attr_type + ", "
+    elif c_infos[info] is None:
+        attr_type = 'VARCHAR'
+        to_query += attr + " " + attr_type + ", "
+    # elif type(c_infos[info]) == dict:
+    #     attr_type = 'Dict'
+    #     to_query += attr + " " + attr_type + ", "
+    # elif type(c_infos[info]) == list:
+    #     attr_type = 'List'
+    #     to_query += attr + " " + attr_type + ", "
+
+to_query = to_query[:-2]
+query = "CREATE TABLE " + "candidatos (" + to_query + ");"
 
 try:
     conn = psycopg2.connect(
@@ -19,8 +52,7 @@ except:
 
 cur = conn.cursor()
 try:
-    cur.execute(
-        "CREATE TABLE testando (id serial PRIMARY KEY, num integer, data varchar);")
+    cur.execute(query)
     print("Query Success")
 except:
     print("Query Failed")
